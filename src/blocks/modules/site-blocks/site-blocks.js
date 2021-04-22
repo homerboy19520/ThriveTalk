1b38d09361819defe87260eae94eff48bc66df8e
Артур Филимонцев <homerboy19520@mail.ru>
2021-04-22 18:53:56 +0300

    Запушил блоки

---
+++ src/blocks/modules/site-blocks/site-blocks.js
@@ -0,0 +1,13 @@
+modules.define('site-blocks', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
